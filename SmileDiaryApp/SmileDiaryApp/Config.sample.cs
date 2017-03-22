729515275e6a27926eed60b75f68f406a9889edb
wellwind <wellwind@mail2000.com.tw>
2017-03-22 21:07:49 +0800

    加入EmotionApi套件及設定檔範例

---
+++ SmileDiaryApp/SmileDiaryApp/Config.sample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmileDiaryApp
+{
+    public static class Config
+    {
+        public static readonly string  EmotionApiKey= "[your emotion api key]";
+    }
+}
