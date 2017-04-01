f4cf364eaee0cefabe520a94f7b2f563093cb2ff
wellwind <wellwind@mail2000.com.tw>
2017-04-01 22:03:35 +0800

    [Badge] 連續三天記錄微笑

---
+++ SmileDiaryApp/SmileDiaryApp/BadgeCheckers/Badge002_3DaysRecordChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmileDiaryApp.BadgeCheckers
+{
+    public class Badge002_3DaysRecordChecker : IBadgeChecker
+    {
+        public void Check(double score, BadgeData badgeData)
+        {
+            if (!badgeData.Badge002_3DaysRecord)
+            {
+                if (badgeData.RecordDays >= 3)
+                {
+                    badgeData.Badge002_3DaysRecord = true;
+                }
+            }
+        }
+    }
+}
